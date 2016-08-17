6bd133f2c662199db80d81c43b8dd9004fb4cf5a
Karolina <karolina.rostek.93@gmail.com>
2016-08-17 17:24:56 +0200

    add user model and user tests

---
+++ organizer/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true,
+                  length: { minimum: 3, maximum: 50 }
+  validates :email, presence: true,
+                  length: { minimum: 7, maximum: 50 },
+                  format: { with: VALID_EMAIL_REGEX }
+end
---
+++ organizer/test/models/user_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: "aaa", email: "aaa@aaa.com")
+  end
+
+  test"should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too short" do
+    @user.name = "a"
+    assert_not @user.valid?
+  end
+
+  test "email should not be too short" do
+    @user.email = "a@a.a"
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 50 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid address" do
+    valid_address = "ade1a2@aaa.aa"
+    @user.email = valid_address
+    assert @user.valid?, "#{valid_address.inspect} should be valid"
+  end
+
+  test "email validation should not accept invalid address" do
+    invalid_address = "aa.aa"
+    @user.email = invalid_address
+    assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+  end
+end
