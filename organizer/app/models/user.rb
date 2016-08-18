5a097e959d7a420291dfaf0171ccdf4000479813
Karolina <karolina.rostek.93@gmail.com>
2016-08-18 17:04:48 +0200

    add password to user

--- organizer/app/models/user.rb
+++ organizer/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   before_save { email.downcase! }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  has_secure_password
 
   validates :name, presence: true,
                   length: { minimum: 3, maximum: 50 }
@@ -8,4 +9,7 @@ class User < ApplicationRecord
                   length: { minimum: 7, maximum: 50 },
                   format: { with: VALID_EMAIL_REGEX },
                   uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true,
+                      length: { minimum: 6 }
 end
--- organizer/db/schema.rb
+++ organizer/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817135833) do
+ActiveRecord::Schema.define(version: 20160818143736) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
