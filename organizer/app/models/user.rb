d4f23aa9bf9a9cf5573f215835b7eacf49af7942
Karolina <karolina.rostek.93@gmail.com>
2016-08-18 21:26:04 +0200

    add devise

--- organizer/app/models/user.rb
+++ organizer/app/models/user.rb
@@ -1,15 +1,8 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
-  validates :name, presence: true,
-                  length: { minimum: 3, maximum: 50 }
-  validates :email, presence: true,
-                  length: { minimum: 7, maximum: 50 },
-                  format: { with: VALID_EMAIL_REGEX },
-                  uniqueness: { case_sensitive: false }
 
-  validates :password, presence: true,
-                      length: { minimum: 6 }
 end
