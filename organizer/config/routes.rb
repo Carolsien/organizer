e6e10624a80e3d01977352ebfcdba70212dca697
Karolina <karolina.rostek.93@gmail.com>
2016-08-22 23:27:57 +0200

    change routes

--- organizer/config/routes.rb
+++ organizer/config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
 
   get   'help'    => 'static_pages#help'
   get   'about'   => 'static_pages#about'
-  get   'signup'  => 'users/sign_up'
-  get   'signin'  => 'users/sign_in'
+
   devise_for :users, controllers: {
              registrations: 'users/registrations',
              confirmations: 'users/confirmations',
